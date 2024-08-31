a0159d39228f5d7ea50616cab490a964d567bf4b
Nhật Huy ★彡 <provcc143300@gmail.com>
2024-08-31 12:51:52 +0700

    Update train_finetune.py

--- train_finetune.py
+++ train_finetune.py
@@ -88,7 +88,7 @@ def main(config_path):
     optimizer_params = Munch(config['optimizer_params'])
     
     train_list, val_list = get_data_path_list(train_path, val_path)
-    device = 'cuda'
+    device = 'cpu' if not torch.cuda.is_available() else 'cuda'
 
     train_dataloader = build_dataloader(train_list,
                                         root_path,
